3182786e19d032e32a063299b8a2e865f1e6c879
Himanshu Mehra <himanshumehra@Himanshus-MacBook-Air.local>
2024-12-19 23:18:20 +0530

    initial commit

--- src/App.js
+++
@@ -1,6 +0,0 @@
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-import Sidebar from "./components/Sidebar";
-const App = () => {
-    return (_jsxs("div", { style: { display: "flex" }, children: [_jsx(Sidebar, {}), _jsx("div", { style: { padding: "16px", flex: 1 } })] }));
-};
-export default App;
